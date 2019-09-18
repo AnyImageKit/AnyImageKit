381630c4296002c1c933acf6bec6c62fdfc33f8d
liudong <liudong.edward@gmail.com>
2019-09-18 10:58:59 +0800

    fix build error

--- Sources/Extension/Ex+UIColor.swift
+++ Sources/Extension/Ex+UIColor.swift
@@ -10,12 +10,12 @@ import UIKit
 
 extension UIColor {
     
-    public static func color(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
+    static func color(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
         let divisor = CGFloat(255)
         return UIColor(red: r/divisor, green: g/divisor, blue: b/divisor, alpha: a)
     }
     
-    public static func color(hex: UInt32, alpha: CGFloat = 1.0) -> UIColor {
+    static func color(hex: UInt32, alpha: CGFloat = 1.0) -> UIColor {
         let r = CGFloat((hex & 0xFF0000) >> 16)
         let g = CGFloat((hex & 0x00FF00) >> 8)
         let b = CGFloat((hex & 0x0000FF))
