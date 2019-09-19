ac8c1987c90732e670f776b3c8d9fe7c3cb83a40
RayJiang <1184731421@qq.com>
2019-09-19 17:52:38 +0800

    修改 cell 的样式

--- Sources/Core/AssetPickerViewController.swift
+++ Sources/Core/AssetPickerViewController.swift
@@ -128,6 +128,7 @@ extension AssetPickerViewController: UICollectionViewDataSource {
         if let asset = album?.assets[indexPath.item] {
             cell.setContent(asset)
         }
+        
         cell.backgroundColor = UIColor.white
         return cell
     }
