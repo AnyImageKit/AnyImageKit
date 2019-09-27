c5ce4f4eb451435aca6a2477293165268509d58a
RayJiang <1184731421@qq.com>
2019-09-27 16:48:57 +0800

    封装PreviewCell

--- Sources/View/Cell/PhotoPreviewCell.swift
+++ Sources/View/Cell/PhotoPreviewCell.swift
@@ -8,154 +8,59 @@
 
 import UIKit
 
-protocol PhotoPreviewCellDelegate: class {
-    /// 拖动时回调。scale:缩放比率
-    func previewCell(_ cell: PhotoPreviewCell, didPanScale scale: CGFloat)
+final class PhotoPreviewCell: PreviewCell {
     
-    func previewCell(_ cell: PhotoPreviewCell, didEndPanWithExit isExit: Bool)
-
-    /// 单击时回调
-    func previewCellDidSingleTap(_ cell: PhotoPreviewCell)
-}
-
-final class PhotoPreviewCell: UICollectionViewCell {
+    /// 双击手势
+    lazy var doubleTap: UITapGestureRecognizer = {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(onDoubleTap(_:)))
+        doubleTap.numberOfTapsRequired = 2
+        return doubleTap
+    }()
     
-    public weak var delegate: PhotoPreviewCellDelegate? = nil
-    
-    /// 显示图像
-    public let imageView = UIImageView()
-
     /// 捏合手势放大图片时的最大允许比例
-    public var imageMaximumZoomScale: CGFloat = 2.0 {
+    var imageMaximumZoomScale: CGFloat = 2.0 {
         didSet {
             self.scrollView.maximumZoomScale = imageMaximumZoomScale
         }
     }
 
     /// 双击放大图片时的目标比例
-    public var imageZoomScaleForDoubleTap: CGFloat = 2.0
+    var imageZoomScaleForDoubleTap: CGFloat = 2.0
 
-    /// 内嵌容器。本类不能继承UIScrollView。
-    /// 因为实测UIScrollView遵循了UIGestureRecognizerDelegate协议，而本类也需要遵循此协议，
-    /// 若继承UIScrollView则会覆盖UIScrollView的协议实现，故只内嵌而不继承。
-    public let scrollView = UIScrollView()
-
-    /// 计算contentSize应处于的中心位置
-    private var centerOfContentSize: CGPoint {
-        let deltaWidth = bounds.width - scrollView.contentSize.width
-        let offsetX = deltaWidth > 0 ? deltaWidth * 0.5 : 0
-        let deltaHeight = bounds.height - scrollView.contentSize.height
-        let offsetY = deltaHeight > 0 ? deltaHeight * 0.5 : 0
-        return CGPoint(x: scrollView.contentSize.width * 0.5 + offsetX,
-                       y: scrollView.contentSize.height * 0.5 + offsetY)
-    }
-
-    /// 取图片适屏size
-    private var fitSize: CGSize {
-        guard let image = imageView.image else { return CGSize.zero }
-        let width = scrollView.bounds.width
-        let scale = image.size.height / image.size.width
-        var size = CGSize(width: width, height: scale * width)
-        let screenSize = UIScreen.main.bounds.size
-        if size.width > size.height {
-            size.width = size.width * screenSize.height / size.height
-            size.height = screenSize.height
-        }
-        return size
-    }
-
-    /// 取图片适屏frame
-    private var fitFrame: CGRect {
-        let size = fitSize
-        let y = (scrollView.bounds.height - size.height) > 0 ? (scrollView.bounds.height - size.height) * 0.5 : 0
-        return CGRect(x: 0, y: y, width: size.width, height: size.height)
-    }
-
-    /// 记录pan手势开始时imageView的位置
-    private var beganFrame = CGRect.zero
-
-    /// 记录pan手势开始时，手势位置
-    private var beganTouch = CGPoint.zero
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor.clear
         setupView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func setupView() {
-        contentView.addSubview(scrollView)
-        scrollView.delegate = self
-        scrollView.maximumZoomScale = imageMaximumZoomScale
-        scrollView.showsVerticalScrollIndicator = false
-        scrollView.showsHorizontalScrollIndicator = false
-        if #available(iOS 11.0, *) {
-            scrollView.contentInsetAdjustmentBehavior = .never
-        }
-
-        scrollView.addSubview(imageView)
-        imageView.clipsToBounds = true
-
-        // 双击手势
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(onDoubleTap(_:)))
-        doubleTap.numberOfTapsRequired = 2
-        contentView.addGestureRecognizer(doubleTap)
-
-        // 单击手势
-        let singleTap = UITapGestureRecognizer(target: self, action: #selector(onSingleTap))
-        contentView.addGestureRecognizer(singleTap)
-        singleTap.require(toFail: doubleTap)
-
-        // 拖动手势
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
-        pan.delegate = self
-        // 必须加在scrollView上。不能加在contentView上，否则长图下拉不能触发
-        scrollView.addGestureRecognizer(pan)
-    }
-    
-    private func layout() {
-        scrollView.frame = contentView.bounds
-        scrollView.setZoomScale(1.0, animated: false)
-        imageView.frame = fitFrame
-        scrollView.minimumZoomScale = getDefaultScale()
-        scrollView.setZoomScale(scrollView.minimumZoomScale, animated: false)
-    }
-    
-    private func getDefaultScale() -> CGFloat {
-        guard let image = imageView.image else { return 1.0 }
-        let width = scrollView.bounds.width
-        let scale = image.size.height / image.size.width
-        let size = CGSize(width: width, height: scale * width)
-        let screenSize = UIScreen.main.bounds.size
-        if size.width > size.height {
-            return size.height / screenSize.height
-        }
-        return 1.0
-    }
 }
 
+// MARK: - Public function
 extension PhotoPreviewCell {
-    public func setImage(_ image: UIImage?) {
-        imageView.image = image
-        layout()
-    }
     
     public func reset() {
         scrollView.setZoomScale(scrollView.minimumZoomScale, animated: false)
     }
 }
 
+// MARK: - Private function
+extension PhotoPreviewCell {
+    
+    private func setupView() {
+        scrollView.delegate = self
+        scrollView.maximumZoomScale = imageMaximumZoomScale
+
+        // 双击手势
+        contentView.addGestureRecognizer(doubleTap)
+        singleTap.require(toFail: doubleTap)
+    }
+}
+
 // MARK: - Target
 extension PhotoPreviewCell {
-    /// 响应单击
-    @objc private func onSingleTap() {
-        delegate?.previewCellDidSingleTap(self)
-    }
-
+    
     /// 响应双击
     @objc private func onDoubleTap(_ dbTap: UITapGestureRecognizer) {
         // 如果当前没有任何缩放，则放大到目标比例
@@ -178,108 +83,16 @@ extension PhotoPreviewCell {
             scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
         }
     }
-
-    /// 响应拖动
-    @objc private func onPan(_ pan: UIPanGestureRecognizer) {
-        guard imageView.image != nil else {
-            return
-        }
-        switch pan.state {
-        case .began:
-            beganFrame = imageView.frame
-            beganTouch = pan.location(in: scrollView)
-        case .changed:
-            let result = panResult(pan)
-            imageView.frame = result.0
-            // 通知代理，发生了缩放。代理可依scale值改变背景蒙板alpha值
-            delegate?.previewCell(self, didPanScale: result.1)
-        case .ended, .cancelled:
-            imageView.frame = panResult(pan).0
-            if pan.velocity(in: self).y > 0 {
-                // dismiss
-                delegate?.previewCell(self, didEndPanWithExit: true)
-            } else {
-                // 取消dismiss
-                endPan()
-            }
-        default:
-            endPan()
-        }
-    }
-
-    private func panResult(_ pan: UIPanGestureRecognizer) -> (CGRect, CGFloat) {
-        // 拖动偏移量
-        let translation = pan.translation(in: scrollView)
-        let currentTouch = pan.location(in: scrollView)
-
-        // 由下拉的偏移值决定缩放比例，越往下偏移，缩得越小。scale值区间[0.3, 1.0]
-        let scale = min(1.0, max(0.3, 1 - translation.y / bounds.height))
-
-        let width = beganFrame.size.width * scale
-        let height = beganFrame.size.height * scale
-
-        // 计算x和y。保持手指在图片上的相对位置不变。
-        // 即如果手势开始时，手指在图片X轴三分之一处，那么在移动图片时，保持手指始终位于图片X轴的三分之一处
-        let xRate = (beganTouch.x - beganFrame.origin.x) / beganFrame.size.width
-        let currentTouchDeltaX = xRate * width
-        let x = currentTouch.x - currentTouchDeltaX
-
-        let yRate = (beganTouch.y - beganFrame.origin.y) / beganFrame.size.height
-        let currentTouchDeltaY = yRate * height
-        let y = currentTouch.y - currentTouchDeltaY
-
-        return (CGRect(x: x.isNaN ? 0 : x, y: y.isNaN ? 0 : y, width: width, height: height), scale)
-    }
-
-    private func endPan() {
-        delegate?.previewCell(self, didPanScale: 1.0)
-        delegate?.previewCell(self, didEndPanWithExit: false)
-        // 如果图片当前显示的size小于原size，则重置为原size
-        let size = fitSize
-        let needResetSize = imageView.bounds.size.width < size.width
-            || imageView.bounds.size.height < size.height
-        UIView.animate(withDuration: 0.25) {
-            self.imageView.center = self.centerOfContentSize
-            if needResetSize {
-                self.imageView.bounds.size = size
-            }
-        }
-    }
 }
 
 // MARK: - UIScrollViewDelegate
 extension PhotoPreviewCell: UIScrollViewDelegate {
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
 
     func scrollViewDidZoom(_ scrollView: UIScrollView) {
         imageView.center = centerOfContentSize
-    }
-}
-
-// MARK: - UIGestureRecognizerDelegate
-extension PhotoPreviewCell: UIGestureRecognizerDelegate {
-    
-    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        // 只响应pan手势
-        guard let pan = gestureRecognizer as? UIPanGestureRecognizer else {
-            return true
-        }
-        let velocity = pan.velocity(in: self)
-        // 向上滑动时，不响应手势
-        if velocity.y < 0 {
-            return false
-        }
-        // 横向滑动时，不响应pan手势
-        if abs(Int(velocity.x)) > Int(velocity.y) {
-            return false
-        }
-        // 向下滑动，如果图片顶部超出可视区域，不响应手势
-        if scrollView.contentOffset.y > 0 {
-            return false
-        }
-        // 响应允许范围内的下滑手势
-        return true
     }
 }
---
+++ Sources/View/Cell/PreviewCell.swift
@@ -0,0 +1,249 @@
+//
+//  PreviewCell.swift
+//  AnyImagePicker
+//
+//  Created by 蒋惠 on 2019/9/27.
+//  Copyright © 2019 anotheren.com. All rights reserved.
+//
+
+import UIKit
+
+protocol PreviewCellDelegate: class {
+    /// 拖动时回调。scale:缩放比率
+    func previewCell(_ cell: PreviewCell, didPanScale scale: CGFloat)
+    
+    func previewCell(_ cell: PreviewCell, didEndPanWithExit isExit: Bool)
+
+    /// 单击时回调
+    func previewCellDidSingleTap(_ cell: PreviewCell)
+}
+
+class PreviewCell: UICollectionViewCell {
+    
+    public weak var delegate: PreviewCellDelegate? = nil
+    
+    /// 内嵌容器。本类不能继承UIScrollView。
+    /// 因为实测UIScrollView遵循了UIGestureRecognizerDelegate协议，而本类也需要遵循此协议，
+    /// 若继承UIScrollView则会覆盖UIScrollView的协议实现，故只内嵌而不继承。
+    lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.showsVerticalScrollIndicator = false
+        view.showsHorizontalScrollIndicator = false
+        if #available(iOS 11.0, *) {
+            view.contentInsetAdjustmentBehavior = .never
+        }
+        return view
+    }()
+    
+    /// 显示图像
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    /// 单击手势
+    lazy var singleTap: UITapGestureRecognizer = {
+        return UITapGestureRecognizer(target: self, action: #selector(onSingleTap))
+    }()
+    
+    /// 拖动手势
+    lazy var pan: UIPanGestureRecognizer = {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        pan.delegate = self
+        return pan
+    }()
+    
+    /// 计算contentSize应处于的中心位置
+    var centerOfContentSize: CGPoint {
+        let deltaWidth = bounds.width - scrollView.contentSize.width
+        let offsetX = deltaWidth > 0 ? deltaWidth * 0.5 : 0
+        let deltaHeight = bounds.height - scrollView.contentSize.height
+        let offsetY = deltaHeight > 0 ? deltaHeight * 0.5 : 0
+        return CGPoint(x: scrollView.contentSize.width * 0.5 + offsetX,
+                       y: scrollView.contentSize.height * 0.5 + offsetY)
+    }
+
+    /// 取图片适屏size
+    var fitSize: CGSize {
+        guard let image = imageView.image else { return CGSize.zero }
+        let width = scrollView.bounds.width
+        let scale = image.size.height / image.size.width
+        var size = CGSize(width: width, height: scale * width)
+        let screenSize = UIScreen.main.bounds.size
+        if size.width > size.height {
+            size.width = size.width * screenSize.height / size.height
+            size.height = screenSize.height
+        }
+        return size
+    }
+
+    /// 取图片适屏frame
+    var fitFrame: CGRect {
+        let size = fitSize
+        let y = (scrollView.bounds.height - size.height) > 0 ? (scrollView.bounds.height - size.height) * 0.5 : 0
+        return CGRect(x: 0, y: y, width: size.width, height: size.height)
+    }
+
+    /// 记录pan手势开始时imageView的位置
+    private var beganFrame = CGRect.zero
+
+    /// 记录pan手势开始时，手势位置
+    private var beganTouch = CGPoint.zero
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Public function
+extension PreviewCell {
+    
+    public func setImage(_ image: UIImage?) {
+        imageView.image = image
+        layout()
+    }
+}
+
+// MARK: - Private function
+extension PreviewCell {
+    
+    private func setupView() {
+        contentView.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+
+        // 添加手势
+        contentView.addGestureRecognizer(singleTap)
+        // 必须加在scrollView上。不能加在contentView上，否则长图下拉不能触发
+        scrollView.addGestureRecognizer(pan)
+    }
+    
+    /// 重新布局
+    private func layout() {
+        scrollView.frame = contentView.bounds
+        scrollView.setZoomScale(1.0, animated: false)
+        imageView.frame = fitFrame
+        scrollView.minimumZoomScale = getDefaultScale()
+        scrollView.setZoomScale(scrollView.minimumZoomScale, animated: false)
+    }
+    
+    /// 获取缩放比例
+    private func getDefaultScale() -> CGFloat {
+        guard let image = imageView.image else { return 1.0 }
+        let width = scrollView.bounds.width
+        let scale = image.size.height / image.size.width
+        let size = CGSize(width: width, height: scale * width)
+        let screenSize = UIScreen.main.bounds.size
+        if size.width > size.height {
+            return size.height / screenSize.height
+        }
+        return 1.0
+    }
+}
+
+// MARK: - Target
+extension PreviewCell {
+    /// 响应单击
+    @objc private func onSingleTap() {
+        delegate?.previewCellDidSingleTap(self)
+    }
+
+    /// 响应拖动
+    @objc private func onPan(_ pan: UIPanGestureRecognizer) {
+        guard imageView.image != nil else {
+            return
+        }
+        switch pan.state {
+        case .began:
+            beganFrame = imageView.frame
+            beganTouch = pan.location(in: scrollView)
+        case .changed:
+            let result = panResult(pan)
+            imageView.frame = result.0
+            // 通知代理，发生了缩放。代理可依scale值改变背景蒙板alpha值
+            delegate?.previewCell(self, didPanScale: result.1)
+        case .ended, .cancelled:
+            imageView.frame = panResult(pan).0
+            if pan.velocity(in: self).y > 0 {
+                // dismiss
+                delegate?.previewCell(self, didEndPanWithExit: true)
+            } else {
+                // 取消dismiss
+                endPan()
+            }
+        default:
+            endPan()
+        }
+    }
+
+    private func panResult(_ pan: UIPanGestureRecognizer) -> (CGRect, CGFloat) {
+        // 拖动偏移量
+        let translation = pan.translation(in: scrollView)
+        let currentTouch = pan.location(in: scrollView)
+
+        // 由下拉的偏移值决定缩放比例，越往下偏移，缩得越小。scale值区间[0.3, 1.0]
+        let scale = min(1.0, max(0.3, 1 - translation.y / bounds.height))
+
+        let width = beganFrame.size.width * scale
+        let height = beganFrame.size.height * scale
+
+        // 计算x和y。保持手指在图片上的相对位置不变。
+        // 即如果手势开始时，手指在图片X轴三分之一处，那么在移动图片时，保持手指始终位于图片X轴的三分之一处
+        let xRate = (beganTouch.x - beganFrame.origin.x) / beganFrame.size.width
+        let currentTouchDeltaX = xRate * width
+        let x = currentTouch.x - currentTouchDeltaX
+
+        let yRate = (beganTouch.y - beganFrame.origin.y) / beganFrame.size.height
+        let currentTouchDeltaY = yRate * height
+        let y = currentTouch.y - currentTouchDeltaY
+
+        return (CGRect(x: x.isNaN ? 0 : x, y: y.isNaN ? 0 : y, width: width, height: height), scale)
+    }
+
+    private func endPan() {
+        delegate?.previewCell(self, didPanScale: 1.0)
+        delegate?.previewCell(self, didEndPanWithExit: false)
+        // 如果图片当前显示的size小于原size，则重置为原size
+        let size = fitSize
+        let needResetSize = imageView.bounds.size.width < size.width
+            || imageView.bounds.size.height < size.height
+        UIView.animate(withDuration: 0.25) {
+            self.imageView.center = self.centerOfContentSize
+            if needResetSize {
+                self.imageView.bounds.size = size
+            }
+        }
+    }
+}
+
+// MARK: - UIGestureRecognizerDelegate
+extension PreviewCell: UIGestureRecognizerDelegate {
+    
+    override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        // 只响应pan手势
+        guard let pan = gestureRecognizer as? UIPanGestureRecognizer else {
+            return true
+        }
+        let velocity = pan.velocity(in: self)
+        // 向上滑动时，不响应手势
+        if velocity.y < 0 {
+            return false
+        }
+        // 横向滑动时，不响应pan手势
+        if abs(Int(velocity.x)) > Int(velocity.y) {
+            return false
+        }
+        // 向下滑动，如果图片顶部超出可视区域，不响应手势
+        if scrollView.contentOffset.y > 0 {
+            return false
+        }
+        // 响应允许范围内的下滑手势
+        return true
+    }
+}
