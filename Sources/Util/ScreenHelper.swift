976fed4a8bcde82afe37450344bbe3a7f85a58ac
liudong <liudong.edward@gmail.com>
2019-10-24 16:12:41 +0800

    add ScreenHelper

---
+++ Sources/Util/ScreenHelper.swift
@@ -0,0 +1,27 @@
+//
+//  ScreenHelper.swift
+//  AnyImagePicker
+//
+//  Created by 刘栋 on 2019/10/24.
+//  Copyright © 2019 AnyImageProject.org. All rights reserved.
+//
+
+import UIKit
+
+struct ScreenHelper {
+    
+    static var mainBounds: CGRect {
+        if #available(iOS 13, *) {
+            for scene in UIApplication.shared.connectedScenes {
+                if scene.activationState == .foregroundActive {
+                    if let delegate = scene.delegate as? UIWindowSceneDelegate {
+                        if let frame = delegate.window??.bounds {
+                            return frame
+                        }
+                    }
+                }
+            }
+        }
+        return UIApplication.shared.windows[0].bounds
+    }
+}
