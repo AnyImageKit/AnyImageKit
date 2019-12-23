05401ce8cfd0ead65648ef7631ad1f31097c4360
RayJiang <1184731421@qq.com>
2019-12-23 13:12:26 +0800

    Add progress UI

--- Sources/Editor/View/VideoEditorCropProgressView.swift
+++ Sources/Editor/View/VideoEditorCropProgressView.swift
@@ -10,29 +10,61 @@ import UIKit
 
 final class VideoEditorCropProgressView: UIView {
 
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    private lazy var progressContentView: UIView = {
+        let view = UIView()
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(progressViewPan(_:)))
+        view.addGestureRecognizer(pan)
+        return view
+    }()
+    private lazy var progressView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 2.5
+        view.backgroundColor = UIColor.white
+        return view
+    }()
     private(set) lazy var leftButton: UIButton = {
         let view = UIButton(type: .custom)
         view.setImage(BundleHelper.image(named: "VideoCropLeftWhite"), for: .normal)
         view.setImage(BundleHelper.image(named: "VideoCropLeftBlack"), for: .selected)
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(leftButtonPan(_:)))
+        view.addGestureRecognizer(pan)
         return view
     }()
     private(set) lazy var rightButton: UIButton = {
         let view = UIButton(type: .custom)
         view.setImage(BundleHelper.image(named: "VideoCropRightWhite"), for: .normal)
         view.setImage(BundleHelper.image(named: "VideoCropRightBlack"), for: .selected)
-        return view
-    }()
-    private lazy var progressView: ProgressView = {
-        let view = ProgressView(frame: .zero)
-        let pan = UIPanGestureRecognizer(target: self, action: #selector(progressViewPan(_:)))
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(rightButtonPan(_:)))
         view.addGestureRecognizer(pan)
         return view
+    }()
+    private lazy var contentLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.isHidden = true
+        layer.frame = bounds
+        layer.fillRule = .evenOdd
+        layer.fillColor = UIColor.yellow.cgColor
+        return layer
+    }()
+    private lazy var darkLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.frame = bounds
+        layer.fillRule = .evenOdd
+        layer.fillColor = UIColor.black.withAlphaComponent(0.6).cgColor
+        return layer
     }()
     
     private(set) var left: CGFloat = 0
     private(set) var right: CGFloat = 1
     
+    /// 预览图
     private var previews: [UIImageView] = []
+    private var previewStackView: UIStackView?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -50,10 +82,19 @@ final class VideoEditorCropProgressView: UIView {
     }
     
     private func setupView() {
-        addSubview(progressView)
-        addSubview(leftButton)
-        addSubview(rightButton)
+        layer.addSublayer(darkLayer)
+        addSubview(contentView)
+        contentView.addSubview(progressContentView)
+        contentView.layer.addSublayer(contentLayer)
+        progressContentView.addSubview(progressView)
+        contentView.addSubview(leftButton)
+        contentView.addSubview(rightButton)
         
+        contentView.snp.makeConstraints { (maker) in
+            maker.top.bottom.equalToSuperview()
+            maker.left.equalToSuperview()
+            maker.right.equalToSuperview()
+        }
         leftButton.snp.makeConstraints { (maker) in
             maker.top.bottom.equalToSuperview()
             maker.left.equalToSuperview()
@@ -64,20 +105,55 @@ final class VideoEditorCropProgressView: UIView {
             maker.right.equalToSuperview()
             maker.width.equalTo(20)
         }
+        progressContentView.snp.makeConstraints { (maker) in
+            maker.top.bottom.equalToSuperview()
+            maker.left.equalTo(leftButton.snp.right)
+            maker.right.equalTo(rightButton.snp.left)
+        }
         progressView.snp.makeConstraints { (maker) in
             maker.top.bottom.equalToSuperview().inset(3)
-            maker.width.equalTo(20)
-            maker.left.equalToSuperview().offset(20-7.5)
+            maker.width.equalTo(5)
+            maker.left.equalToSuperview()
         }
     }
     
     private func layout() {
-        leftButton.snp.updateConstraints { (maker) in
+        let isSelected = right - left != 1
+        leftButton.isSelected = isSelected
+        rightButton.isSelected = isSelected
+        contentLayer.isHidden = !isSelected
+        
+        contentView.snp.updateConstraints { (maker) in
             maker.left.equalToSuperview().offset(left*bounds.width)
         }
-        rightButton.snp.updateConstraints { (maker) in
+        contentView.snp.updateConstraints { (maker) in
             maker.right.equalToSuperview().offset(-(1-right)*bounds.width)
         }
+        progressView.snp.updateConstraints { (maker) in
+            maker.left.equalToSuperview()
+        }
+        contentLayer.frame = contentView.bounds
+        updateContentLayer()
+        updateDarkLayer()
+    }
+    
+    private func updateContentLayer() {
+        let contentPath = UIBezierPath(roundedRect: contentView.bounds, cornerRadius: 5)
+        var rect = contentView.bounds
+        rect.origin.x += 20
+        rect.origin.y += 5
+        rect.size.width -= 40
+        rect.size.height -= 10
+        let rectPath = UIBezierPath(rect: rect)
+        contentPath.append(rectPath)
+        contentLayer.path = contentPath.cgPath
+    }
+    
+    private func updateDarkLayer() {
+        let darkPath = UIBezierPath(rect: bounds)
+        let rectPath = UIBezierPath(rect: contentView.frame)
+        darkPath.append(rectPath)
+        darkLayer.path = darkPath.cgPath
     }
 }
 
@@ -91,6 +167,7 @@ extension VideoEditorCropProgressView {
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         stackView.alignment = .fill
+        previewStackView = stackView
         insertSubview(stackView, at: 0)
         stackView.snp.makeConstraints { (maker) in
             maker.top.bottom.equalToSuperview().inset(5)
@@ -108,45 +185,36 @@ extension VideoEditorCropProgressView {
 extension VideoEditorCropProgressView {
     
     @objc private func progressViewPan(_ pan: UIPanGestureRecognizer) {
-        pan.location(in: <#T##UIView?#>)
-        let newPoint = pan.translation(in: self)
-        pan.setTranslation(.zero, in: self)
-        let offset = progressView.frame.origin.x + newPoint.x
-        if offset < 20-7.5 || offset > bounds.width-40+7.5 {
+        guard let stackView = previewStackView else { return }
+        let point = pan.location(in: stackView)
+        if point.x < contentView.frame.origin.x + 20 || point.x > contentView.frame.maxX - 20 {
             return
         }
+        let offset = point.x - contentView.frame.origin.x - 20
         progressView.snp.updateConstraints { (maker) in
             maker.left.equalToSuperview().offset(offset)
         }
     }
-}
-
-
-// MARK: - ProgressView
-private final class ProgressView: UIView {
     
-    private(set) lazy var whiteView: UIView = {
-        let view = UIView()
-        view.layer.cornerRadius = 2.5
-        view.backgroundColor = UIColor.white
-        return view
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupView()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupView() {
-        addSubview(whiteView)
-        whiteView.snp.makeConstraints { (maker) in
-            maker.top.bottom.equalToSuperview()
-            maker.width.equalTo(5)
-            maker.centerX.equalToSuperview()
+    @objc private func leftButtonPan(_ pan: UIPanGestureRecognizer) {
+        let point = pan.location(in: self)
+        let x = point.x < 0 ? 0 : point.x
+        let tmpLeft = x / bounds.width
+        if right - tmpLeft < 0.2 {
+            return
         }
+        left = tmpLeft
+        layout()
+    }
+    
+    @objc private func rightButtonPan(_ pan: UIPanGestureRecognizer) {
+        let point = pan.location(in: self)
+        let x = point.x > bounds.width ? bounds.width : point.x
+        let tmpRight = x / bounds.width
+        if tmpRight - left < 0.2 {
+            return
+        }
+        right = tmpRight
+        layout()
     }
 }
