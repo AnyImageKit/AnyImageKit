7426bf3d1a0da8fc8f1565756c4eaf3c69a0dff2
liudong <liudong.edward@gmail.com>
2020-01-03 13:52:48 +0800

    修复首帧可能黑屏的问题

--- Sources/Capture/Util/Recorder.swift
+++ Sources/Capture/Util/Recorder.swift
@@ -28,11 +28,6 @@ final class Recorder {
     
     private var writer: AVAssetWriter?
     private var writerInputs: [AVMediaType: AVAssetWriterInput] = [:]
-    
-    private var sourceTime: CMTime = .zero
-    private var duration: Int64 = 0
-    private var rotateTime: CMTime = .zero
-    private var clockReference: AVMediaType = .video
 }
 
 extension Recorder {
@@ -46,18 +41,24 @@ extension Recorder {
         workQueue.async {
             guard self.isRunning else { return }
             
-            self.rotateFile(with: sampleBuffer.presentationTimeStamp, mediaType: mediaType)
+            if self.writer == nil, mediaType == .video {
+                self.writer = self.createWriter()
+            }
             
-            guard let writer = self.writer, let input = self.getWriterInput(mediaType: mediaType, sourceFormatHint: sampleBuffer.formatDescription) else {
+            guard let writer = self.writer else {
                 return
             }
+            
+            let input = self.getWriterInput(for: mediaType, format: sampleBuffer.formatDescription)
             
             guard self.isReadyForStartWriting else { return }
             
             switch writer.status {
             case .unknown:
-                writer.startWriting()
-                writer.startSession(atSourceTime: self.sourceTime)
+                if mediaType == .video {
+                    writer.startWriting()
+                    writer.startSession(atSourceTime: sampleBuffer.presentationTimeStamp)
+                }
             default:
                 break
             }
@@ -101,9 +102,6 @@ extension Recorder {
             guard let self = self else { return }
             self.writer = nil
             self.writerInputs.removeAll()
-            self.sourceTime = .zero
-            self.duration = 0
-            self.rotateTime = .zero
             
             let outputURL = writer.outputURL
             let thumbnail = self.createThumbnail(at: outputURL)
@@ -114,26 +112,11 @@ extension Recorder {
         }
     }
     
-    private func rotateFile(with presentationTimeStamp: CMTime, mediaType: AVMediaType) {
-        guard clockReference == mediaType && rotateTime.value < presentationTimeStamp.value else {
-            return
-        }
-        if writer != nil {
-            finishWriting()
-        }
-        writer = createWriter()
-        rotateTime = CMTimeAdd(
-            presentationTimeStamp,
-            CMTimeMake(value: duration == 0 ? .max : duration * Int64(presentationTimeStamp.timescale), timescale: presentationTimeStamp.timescale)
-        )
-        sourceTime = presentationTimeStamp
-    }
-    
-    private func getWriterInput(mediaType: AVMediaType, sourceFormatHint: CMFormatDescription?) -> AVAssetWriterInput? {
+    private func getWriterInput(for mediaType: AVMediaType, format: CMFormatDescription?) -> AVAssetWriterInput {
         if let writerInput = writerInputs[mediaType] {
             return writerInput
         } else {
-            var outputSettings: [String: Any]?
+            var outputSettings: [String: Any]? = nil
             switch mediaType {
             case .audio:
                 outputSettings = preferredAudioSettings
@@ -142,11 +125,13 @@ extension Recorder {
             default:
                 break
             }
-            let writerInput = AVAssetWriterInput(mediaType: mediaType, outputSettings: outputSettings, sourceFormatHint: sourceFormatHint)
-            writerInput.expectsMediaDataInRealTime = true
-            writerInputs[mediaType] = writerInput
-            writer?.add(writerInput)
-            return writerInput
+            let input = AVAssetWriterInput(mediaType: mediaType, outputSettings: outputSettings, sourceFormatHint: format)
+            input.expectsMediaDataInRealTime = true
+            if let writer = writer, writer.canAdd(input) {
+                writerInputs[mediaType] = input
+                writer.add(input)
+            }
+            return input
         }
     }
     
@@ -167,7 +152,7 @@ extension Recorder {
         let asset = AVURLAsset(url: url)
         let imageGenerator = AVAssetImageGenerator(asset: asset)
         imageGenerator.appliesPreferredTrackTransform = true
-        let time = CMTime(seconds: 0.1, preferredTimescale: 1000_000_000)
+        let time = CMTime(seconds: 0.0, preferredTimescale: 1_000_000_000)
         do {
             let cgImage = try imageGenerator.copyCGImage(at: time, actualTime: nil)
             return UIImage(cgImage: cgImage)
