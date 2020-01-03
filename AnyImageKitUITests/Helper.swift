485e027aab015aa7ca986c38c75bae632039f84e
RayJiang <1184731421@qq.com>
2020-01-03 14:41:25 +0800

    Add picker UITest

---
+++ AnyImageKitUITests/Helper.swift
@@ -0,0 +1,13 @@
+//
+//  Helper.swift
+//  AnyImageKitUITests
+//
+//  Created by 蒋惠 on 2020/1/3.
+//  Copyright © 2020 AnyImageProject.org. All rights reserved.
+//
+
+import Foundation
+
+func sleep(_ time: TimeInterval) {
+    Thread.sleep(forTimeInterval: time)
+}
