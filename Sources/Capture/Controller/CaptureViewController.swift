ea38a491b44c776e98c356dbf7bef718ee67eaca
RayJiang <1184731421@qq.com>
2020-01-10 10:08:23 +0800

    Capture 修改提示时机

--- Sources/Capture/Controller/CaptureViewController.swift
+++ Sources/Capture/Controller/CaptureViewController.swift
@@ -85,8 +85,8 @@ final class CaptureViewController: AnyImageViewController {
         })
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         tipsView.showTips(hideAfter: 3, animated: true)
     }
     
