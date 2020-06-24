98bc8a717edd4ca4f681af96c8f25029eb62f3bb
liudong <liudong.edward@gmail.com>
2020-06-24 15:59:37 +0800

    fix Compiler error for Xcode 12 beta 1

--- Sources/Editor/Extension/Editor+UIView.swift
+++ Sources/Editor/Extension/Editor+UIView.swift
@@ -20,14 +20,8 @@ extension UIView {
             format.scale = imageSize.width / size.width
             renderer = UIGraphicsImageRenderer(size: size, format: format)
         }
-        #if swift(>=5.3)
-        return renderer.image { [self] (context) in
-            return layer.render(in: context.cgContext)
+        return renderer.image { [weak self] context in
+            self?.layer.render(in: context.cgContext)
         }
-        #else
-        return renderer.image { [weak self] (context) in
-            return self?.layer.render(in: context.cgContext)
-        }
-        #endif
     }
 }
