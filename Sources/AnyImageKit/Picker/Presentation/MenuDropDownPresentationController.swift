167b521dd5dc4d189c2a13c27f2588ca15a09759
liudong <liudong.edward@gmail.com>
2020-10-27 16:38:14 +0800

    fix: [Picker] 移除不需要的 opaqueView 圆角

--- Sources/AnyImageKit/Picker/Presentation/MenuDropDownPresentationController.swift
+++ Sources/AnyImageKit/Picker/Presentation/MenuDropDownPresentationController.swift
@@ -71,9 +71,6 @@ final class MenuDropDownPresentationController: UIPresentationController {
             opaqueView.isOpaque = false
             opaqueView.backgroundColor = UIColor.black
             opaqueView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            if cornerRadius > 0 {
-                opaqueView.layer.cornerRadius = cornerRadius
-            }
             self.opaqueView = opaqueView
             // clickable area
             let dimmingView = UIView(frame: containerView.bounds)
