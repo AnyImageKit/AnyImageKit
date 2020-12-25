2cc161b113e8c019fcb6fc74c9f45e580e3d03b7
RayJiang <1184731421@qq.com>
2020-12-25 09:42:23 +0800

    docs: 完善注释；更新Wiki

--- Sources/AnyImageKit/Picker/PickerOptionsInfo.swift
+++ Sources/AnyImageKit/Picker/PickerOptionsInfo.swift
@@ -41,10 +41,6 @@ public struct PickerOptionsInfo {
     /// - Default: false
     public var allowUseOriginalImage: Bool = false
     
-    /// Selection Tap Action 资源列表点击动作
-    /// - Default: Preview
-    public var selectionTapAction: PickerSelectionTapAction = .preview
-    
     /// Album Options 相册类型
     /// - Default: smart album + user create album
     public var albumOptions: PickerAlbumOption = [.smart, .userCreated]
@@ -54,6 +50,10 @@ public struct PickerOptionsInfo {
     /// - .photoLive and .photoGIF are subtype of .photo and will be treated as a photo when not explicitly indicated, otherwise special handling will be possible (playable & proprietary)
     /// - .photoLive 和 .photoGIF 是 .photo 的子项，当不显式指明时，都会作为 photo 处理，否则会特殊处理（可播放&专有标识）
     public var selectOptions: PickerSelectOption = [.photo]
+    
+    /// Selection Tap Action 资源列表点击动作
+    /// - Default: Preview
+    public var selectionTapAction: PickerSelectionTapAction = .preview
     
     /// Order by date 按日期排序
     /// - Default: ASC
@@ -162,16 +162,16 @@ public struct PickerEditorOption: OptionSet {
     }
 }
 
-/// Picker Pick Mode 选择类型
-public enum PickerSelectionTapAction {
-    /// Preview
+/// Picker Selection Tap Action 资源列表点击动作
+public enum PickerSelectionTapAction: Equatable {
+    /// Preview 预览
     /// - Default value
     case preview
-    /// Quick 快速选择
+    /// Quick pick 快速选择
     /// - It will select photo instead of show preview controller when you click photo on asset picker controller
     /// - 点击图片时会直接选中该图片，而不会进入预览页面
     case quickPick
-    /// Editor
+    /// Open editor 打开编辑器
     /// - It will open Editor instead of show preview controller when you click photo on asset picker controller
     /// - 点击图片后会进入编辑器，而不会进入预览页面
     case openEditor
