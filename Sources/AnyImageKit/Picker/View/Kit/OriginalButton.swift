7f5215effb42b22019e54530352c3f4b8f6d7b6b
liudong <liudong.edward@gmail.com>
2021-04-01 19:10:12 +0800

    feat: follow WeChat's update for original button

--- Sources/AnyImageKit/Picker/View/Kit/OriginalButton.swift
+++ Sources/AnyImageKit/Picker/View/Kit/OriginalButton.swift
@@ -12,13 +12,22 @@ final class OriginalButton: UIControl {
     
     override var isSelected: Bool {
         didSet {
-            circleView.isSelected = isSelected
+            if isSelected {
+                let image = BundleHelper.image(named: "CheckOn", module: .picker)
+                checkImageView.image = image?.withRenderingMode(.alwaysTemplate)
+            } else {
+                let image = BundleHelper.image(named: "CheckOff", module: .picker)
+                checkImageView.image = image
+            }
         }
     }
     
-    private lazy var circleView: CircleView = {
-        let view = CircleView(frame: .zero, options: options)
+    private lazy var checkImageView: UIImageView = {
+        let view = UIImageView(frame: .zero)
         view.isUserInteractionEnabled = false
+        let image = BundleHelper.image(named: "CheckOff", module: .picker)
+        view.image = image
+        view.tintColor = options.theme.mainColor
         return view
     }()
     private lazy var label: UILabel = {
@@ -46,16 +55,15 @@ final class OriginalButton: UIControl {
     }
     
     private func setupView() {
-        addSubview(circleView)
+        addSubview(checkImageView)
         addSubview(label)
-        circleView.snp.makeConstraints { maker in
+        checkImageView.snp.makeConstraints { maker in
             maker.left.equalToSuperview()
             maker.centerY.equalToSuperview()
-            maker.height.equalTo(self).multipliedBy(0.6)
-            maker.width.equalTo(circleView.snp.height)
+            maker.size.equalTo(CGSize(width: 16, height: 16))
         }
         label.snp.makeConstraints { maker in
-            maker.left.equalTo(circleView.snp.right).offset(5)
+            maker.left.equalTo(checkImageView.snp.right).offset(8)
             maker.right.equalToSuperview()
             maker.centerY.equalToSuperview()
         }
@@ -63,68 +71,5 @@ final class OriginalButton: UIControl {
     
     @objc private func buttonTapped(_ sender: UIButton) {
         isSelected.toggle()
-        circleView.isSelected = isSelected
-    }
-}
-
-fileprivate class CircleView: UIView {
-    
-    var isSelected = false {
-        didSet {
-            smallCircleView.isHidden = !isSelected
-        }
-    }
-    
-    private lazy var bigCircleView: UIView = {
-        let view = UIView(frame: .zero)
-        view.clipsToBounds = true
-        view.layer.borderWidth = 1
-        view.layer.borderColor = options.theme.textColor.cgColor
-        return view
-    }()
-    private lazy var smallCircleView: UIView = {
-        let view = UIView(frame: .zero)
-        view.isHidden = true
-        view.clipsToBounds = true
-        view.backgroundColor = options.theme.mainColor
-        return view
-    }()
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        bigCircleView.layer.cornerRadius = bounds.width * 0.5
-        smallCircleView.layer.cornerRadius = bounds.width * 0.5 - 3
-    }
-    
-    private let options: PickerOptionsInfo
-    
-    init(frame: CGRect, options: PickerOptionsInfo) {
-        self.options = options
-        super.init(frame: frame)
-        setupView()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupView() {
-        addSubview(bigCircleView)
-        addSubview(smallCircleView)
-        bigCircleView.snp.makeConstraints { maker in
-            maker.edges.equalToSuperview()
-        }
-        smallCircleView.snp.makeConstraints { maker in
-            maker.edges.equalToSuperview().inset(3)
-        }
-    }
-    
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        if #available(iOS 13.0, *) {
-            guard options.theme.style == .auto else { return }
-            guard traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) else { return }
-            bigCircleView.layer.borderColor = options.theme.textColor.cgColor
-        }
     }
 }
