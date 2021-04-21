f71af6bbf82e263cfa504217234494d90a3508d8
RayJiang <1184731421@qq.com>
2021-04-21 15:12:46 +0800

    style(Editor): Organize code

--- Sources/AnyImageKit/Editor/Model/ImageEditorCache.swift
+++ Sources/AnyImageKit/Editor/Model/ImageEditorCache.swift
@@ -15,46 +15,21 @@ public final class ImageEditorCache {
 // MARK: - Public static function
 extension ImageEditorCache {
     
-    /// 删除指定磁盘缓存
-    /// - Parameter identifier: 缓存标识符
-    @available(*, deprecated, message: "Will be removed in version 1.0, Please use `removeCache(:)` instead.")
-    public static func clearDiskCache(id: String) {
-        removeCache(identifier: id)
-    }
-    
-    /// 删除指定磁盘缓存
-    /// - Parameter identifier: 缓存标识符
+    /// Delete disk cache file by identifier.
+    /// - Parameter identifier: Cache identifier
     public static func removeCache(identifier: String) {
         let cache = CodableCacheTool(module: .editor(.default))
         guard let model: PhotoEditingStack.Edit = cache.retrieveModel(forKey: identifier) else { return }
-        
-        var pathList = model.brushData.map { $0.drawnPath.uuid }
-        pathList.append(contentsOf: model.mosaicData.flatMap { $0.drawnPaths.map { $0.uuid } })
-        pathList = pathList.map { CacheModule.editor(.bezierPath).path + $0 }
-        let manager = FileManager.default
-        for path in pathList {
-            let url = URL(fileURLWithPath: path)
-            do {
-                try manager.removeItem(at: url)
-            } catch {
-                _print(error.localizedDescription)
-            }
-        }
-        
-        do {
-            try FileManager.default.removeItem(atPath: "\(cache.path)\(identifier)")
-        } catch {
-            _print(error.localizedDescription)
-        }
+        removeImageCache(model: model, mainFilePath: "\(cache.path)\(identifier)")
     }
     
-    /// 删除所有磁盘缓存
+    /// Delete all disk cache
     public static func clearDiskCache() {
         _print("Delete all editor cache")
         clearImageEditorCache()
     }
     
-    /// 删除图片编辑的磁盘缓存
+    /// Delete all Photo Editor disk cache
     public static func clearImageEditorCache() {
         for module in CacheModuleEditor.imageModule {
             let path = CacheModule.editor(module).path
@@ -68,5 +43,42 @@ extension ImageEditorCache {
                 }
             }
         }
+    }
+}
+
+// MARK: - Private static function
+extension ImageEditorCache {
+    
+    private static func removeImageCache(model: PhotoEditingStack.Edit, mainFilePath: String) {
+        var pathList = model.brushData.map { $0.drawnPath.uuid }
+        pathList.append(contentsOf: model.mosaicData.flatMap { $0.drawnPaths.map { $0.uuid } })
+        pathList = pathList.map { CacheModule.editor(.bezierPath).path + $0 }
+        
+        let manager = FileManager.default
+        for path in pathList {
+            let url = URL(fileURLWithPath: path)
+            do {
+                try manager.removeItem(at: url)
+            } catch {
+                _print(error.localizedDescription)
+            }
+        }
+        
+        do {
+            try FileManager.default.removeItem(atPath: mainFilePath)
+        } catch {
+            _print(error.localizedDescription)
+        }
+    }
+}
+
+// MARK: - Deprecated
+extension ImageEditorCache {
+    
+    /// Delete disk cache file by identifier.
+    /// - Parameter id: Cache identifier
+    @available(*, deprecated, renamed: "removeCache(identifier:)", message: "Will be removed in version 1.0, Please use `removeCache(:)` instead.")
+    public static func clearDiskCache(id: String) {
+        removeCache(identifier: id)
     }
 }
