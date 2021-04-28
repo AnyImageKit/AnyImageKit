9cde0de086338787c3e0c877d59ef4d304b0b33b
liudong <liudong.edward@gmail.com>
2021-04-28 18:39:16 +0800

    chore: remove identifier in AssetCell

--- Sources/AnyImageKit/Picker/View/Cell/AssetCell.swift
+++ Sources/AnyImageKit/Picker/View/Cell/AssetCell.swift
@@ -58,11 +58,8 @@ final class AssetCell: UICollectionViewCell {
         return view
     }()
     
-    private var identifier: String = ""
-    
     override func prepareForReuse() {
         super.prepareForReuse()
-        identifier = ""
         selectdCoverView.isHidden = true
         gifView.isHidden = true
         videoView.isHidden = true
@@ -148,12 +145,12 @@ extension AssetCell {
     func setContent(_ asset: PhotoAsset, manager: PickerManager, animated: Bool = false, isPreview: Bool = false) {
         asset.check(disable: manager.options.disableRules)
         let options = _PhotoFetchOptions(sizeMode: .thumbnail(100*UIScreen.main.nativeScale), needCache: false)
-        identifier = asset.identifier
+        let identifier = asset.identifier
         manager.requestPhoto(for: asset.phAsset, options: options, completion: { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let response):
-                guard self.identifier == asset.identifier else { return }
+                guard identifier == asset.identifier else { return }
                 self.imageView.image = asset._image ?? response.image
                 if asset.mediaType == .video && !isPreview {
                     self.videoView.setVideoTime(asset.durationDescription)
